25a4d75195ec0a5f641ebb7c7a1413fa5ace3c49
Filippo Conti <filippo@codekraft.it>
2019-10-13 00:12:10 +0200

    Chore: Updated main file exports
    Export individual components to let them be used singularly in components
    Changed global component registration to pascal case name that includes both names

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import VueRecordAudio from './VueRecordAudio'
 import VueRecordVideo from './VueRecordVideo'
 
+export {
+  VueRecordAudio,
+  VueRecordVideo
+}
+
 export default function install(Vue) {
-  Vue.component('vue-record-audio', VueRecordAudio)
-  Vue.component('vue-record-video', VueRecordVideo)
+  Vue.component('VueRecordAudio', VueRecordAudio)
+  Vue.component('VueRecordVideo', VueRecordVideo)
 }
