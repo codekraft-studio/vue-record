da00e925efacaed48dac06ebfd4872ebb3d072b3
Dominik Fladung <dominik.fladung@fkt42.de>
2020-09-09 13:34:43 +0200

    remove not used class

--- src/components/ElementMixin.js
+++ src/components/ElementMixin.js
@@ -1,39 +1,38 @@
-import RecorderMixin from './RecorderMixin'
-import SafariRecorderMixin from './SafariRecorderMixin'
+import RecorderMixin from './RecorderMixin';
+import SafariRecorderMixin from './SafariRecorderMixin';
 
 const mixins = [RecorderMixin];
 
 if (!window.MediaRecorder) {
-  console.warn('Using Safari polyfill');
-  mixins.push(SafariRecorderMixin)
+	console.warn('Using Safari polyfill');
+	mixins.push(SafariRecorderMixin);
 }
-
 
 /**
  * The element mixin defines the mode behaviour and creates two
  * functions to start and stop the recording execution
  */
 export default {
-  mixins: mixins,
-  props: {
-    mode: {
-      type: String,
-      default: 'hold',
-      validator: v => ['hold', 'press'].includes(v)
-    }
-  },
-  methods: {
-    stopRecording () {
-      if (this.mode === 'press') {
-        return
-      }
-      return this.stop()
-    },
-    startRecording () {
-      if (this.isRecording && this.mode === 'press') {
-        return this.stop()
-      }
-      return this.start()
-    }
-  }
-}
+	mixins: mixins,
+	props: {
+		mode: {
+			type: String,
+			default: 'hold',
+			validator: v => ['hold', 'press'].includes(v),
+		},
+	},
+	methods: {
+		stopRecording() {
+			if (this.mode === 'press') {
+				return;
+			}
+			return this.stop();
+		},
+		startRecording() {
+			if (this.isRecording && this.mode === 'press') {
+				return this.stop();
+			}
+			return this.start();
+		},
+	},
+};
--- src/components/SafariRecorderMixin.js
+++ src/components/SafariRecorderMixin.js
@@ -1,80 +1,77 @@
 import MediaRecorder from 'audio-recorder-polyfill';
 
 export default {
-    methods: {
-        /**
-         * Create a new media recorder with the user media stream
-         * and set some event listeners to update component data
-         * and emit events to the parent component
-         */
-        async prepareRecorder() {
-            if (!this.$_stream) {
-                return;
-            }
+	methods: {
+		/**
+		 * Create a new media recorder with the user media stream
+		 * and set some event listeners to update component data
+		 * and emit events to the parent component
+		 */
+		async prepareRecorder() {
+			if (!this.$_stream) {
+				return;
+			}
 
-            window.MediaRecorder = MediaRecorder;
-            let CustomMediaRecorder = window.MediaRecorder || MediaRecorder;
+			window.MediaRecorder = MediaRecorder;
+			const CustomMediaRecorder = window.MediaRecorder || MediaRecorder;
 
-            this.$_mediaRecorder = new CustomMediaRecorder(
-                this.$_stream,
-                {
-                    mimeType: "audio/wav" // browser dependent
-                }
-            );
+			this.$_mediaRecorder = new CustomMediaRecorder(this.$_stream, {
+				mimeType: 'audio/wav', // browser dependent
+			});
 
-            this.debug && console.log("⏺", this.$_mediaRecorder);
+			this.debug && console.log('⏺', this.$_mediaRecorder);
 
-            this.$_mediaRecorder.ignoreMutedMedia = true;
+			this.$_mediaRecorder.ignoreMutedMedia = true;
 
-            this.$_mediaRecorder.addEventListener("start", () => {
-                this.isRecording = true;
-                this.isPaused = false;
-                this.$emit("start");
-            });
+			this.$_mediaRecorder.addEventListener('start', () => {
+				this.isRecording = true;
+				this.isPaused = false;
+				this.$emit('start');
+			});
 
-            this.$_mediaRecorder.addEventListener("resume", () => {
-                this.isRecording = true;
-                this.isPaused = false;
-                this.$emit("resume");
-            });
+			this.$_mediaRecorder.addEventListener('resume', () => {
+				this.isRecording = true;
+				this.isPaused = false;
+				this.$emit('resume');
+			});
 
-            this.$_mediaRecorder.addEventListener("pause", () => {
-                this.isPaused = true;
-                this.$emit("pause");
-            });
+			this.$_mediaRecorder.addEventListener('pause', () => {
+				this.isPaused = true;
+				this.$emit('pause');
+			});
 
-            // Collect the available data into chunks
-            this.$_mediaRecorder.addEventListener(
-                "dataavailable",
-                e => {
-                    if (e.data && e.data.size > 0) {
-                        this.chunks.push(e.data);
-                    }
-                },
-                true
-            );
+			// Collect the available data into chunks
+			this.$_mediaRecorder.addEventListener(
+				'dataavailable',
+				e => {
+					if (e.data && e.data.size > 0) {
+						this.chunks.push(e.data);
+					}
+				},
+				true,
+			);
 
-            // On recording stop get the data and emit the result
-            // than clear all the recording chunks
-            this.$_mediaRecorder.addEventListener(
-                "stop",
-                () => {
-                    this.$emit("stop");
+			// On recording stop get the data and emit the result
+			// than clear all the recording chunks
+			this.$_mediaRecorder.addEventListener(
+				'stop',
+				() => {
+					this.$emit('stop');
 
-                    const blobData = new Blob(this.chunks, {
-                        type: this.$_mediaRecorder.mimeType
-                    });
-                    if (blobData.size > 0) {
-                        this.$emit("result", blobData);
-                    }
-                    this.chunks = [];
-                    this.isPaused = false;
-                    this.isRecording = false;
+					const blobData = new Blob(this.chunks, {
+						type: this.$_mediaRecorder.mimeType,
+					});
+					if (blobData.size > 0) {
+						this.$emit('result', blobData);
+					}
+					this.chunks = [];
+					this.isPaused = false;
+					this.isRecording = false;
 
-                    this.$_mic.dispose();
-                },
-                true
-            );
-        }
-    },
+					this.$_mic.dispose();
+				},
+				true,
+			);
+		},
+	},
 };
